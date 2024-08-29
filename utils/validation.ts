6eceab662f8fa4cf828b362c51cae7963c75f4fb
rudachenkoev <rudachenkoevgeniy@gmail.com>
2024-08-29 10:04:16 +0200

    update email validation rules

--- utils/validation.ts
+++ utils/validation.ts
@@ -1,6 +1,9 @@
 import { i18n } from '@/libs/i18n'
 
-export const validateEmail = (value: string) => {
+export const validateEmail = (value: unknown) => {
+  if (typeof value !== 'string') {
+    return i18n.t('validator.email')
+  }
   const regex = /\S+@\S+\.\S+/
   return regex.test(value) ? true : i18n.t('validator.email')
 }
