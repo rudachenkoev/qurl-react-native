99b660d70aad16be1ec3c3dc67c95eeb8e52ae8f
rudachenkoev <rudachenkoevgeniy@gmail.com>
2024-09-02 11:58:48 +0200

    update CustomButton theming

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,26 +1,9 @@
 import CustomIcon from '@/components/CustomIcon'
+import colors from '@/constants/colors'
+import { useTheme } from '@/contexts/ThemeContext'
 import { ColorType, CustomButtonProps, VariantType } from '@/types/type'
 import { styled } from 'nativewind'
 import { ActivityIndicator, Pressable, Text } from 'react-native'
-
-const colorStyles: Record<ColorType, Record<VariantType, { wrapper: string; label: string }>> = {
-  primary: {
-    default: { wrapper: 'bg-shark-950 dark:bg-shark-50', label: 'text-shark-50 dark:text-shark-950' },
-    outlined: {
-      wrapper: 'bg-transparent border border-shark-950 dark:border-shark-50',
-      label: 'text-shark-950 dark:text-shark-50'
-    },
-    text: { wrapper: 'bg-transparent', label: 'text-shark-950 dark:text-shark-50' }
-  },
-  secondary: {
-    default: { wrapper: 'bg-shark-400', label: 'text-shark-50' },
-    outlined: { wrapper: 'bg-transparent border border-shark-400', label: 'text-shark-400' },
-    text: { wrapper: 'bg-transparent', label: 'text-shark-400' }
-  }
-}
-
-const getColorStyle = (color: ColorType, variant: VariantType) =>
-  colorStyles[color]?.[variant] || colorStyles.primary.default
 
 const StyledPressable = styled(
   Pressable,
@@ -39,12 +22,33 @@ const CustomButton = ({
   loading = false,
   ...props
 }: CustomButtonProps) => {
+  const { isDarkTheme } = useTheme()
+
+  const colorStyles: Record<ColorType, Record<VariantType, { wrapper: string; label: string }>> = {
+    primary: {
+      default: { wrapper: `bg-shark-${isDarkTheme ? '50' : '950'}`, label: `text-shark-${isDarkTheme ? '950' : '50'}` },
+      outlined: {
+        wrapper: `bg-transparent border border-shark-${isDarkTheme ? '50' : '950'}`,
+        label: `text-shark-${isDarkTheme ? '50' : '950'}`
+      },
+      text: { wrapper: 'bg-transparent', label: `text-shark-${isDarkTheme ? '50' : '950'}` }
+    },
+    secondary: {
+      default: { wrapper: 'bg-shark-400', label: 'text-shark-50' },
+      outlined: { wrapper: 'bg-transparent border border-shark-400', label: 'text-shark-400' },
+      text: { wrapper: 'bg-transparent', label: 'text-shark-400' }
+    }
+  }
+
+  const getColorStyle = (color: ColorType, variant: VariantType) =>
+    colorStyles[color]?.[variant] || colorStyles.primary.default
+
   const { wrapper: wrapperColorClasses, label: labelColorClasses } = getColorStyle(color, variant)
 
   return (
     <StyledPressable className={`${wrapperColorClasses} ${className}`} {...props}>
       {loading ? (
-        <ActivityIndicator color="#fff" />
+        <ActivityIndicator color={colors.shark[50]} />
       ) : (
         <>
           {prependIcon && <CustomIcon {...prependIcon} />}
