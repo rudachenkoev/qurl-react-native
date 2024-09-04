b16a640a3406194fac230349f35d21cc87bdafdb
rudachenkoev <rudachenkoevgeniy@gmail.com>
2024-09-04 12:03:37 +0200

    update colors

--- components/ThemedCard.tsx
+++ components/ThemedCard.tsx
@@ -3,7 +3,7 @@ import { View, ViewProps } from 'react-native'
 
 const ThemedCard = (props: ViewProps) => {
   const { isDarkTheme } = useTheme()
-  return <View className={`rounded-lg p-5 shadow ${isDarkTheme ? 'bg-shark-900' : 'bg-white-lilac-50'}`} {...props} />
+  return <View className={`rounded-lg p-5 shadow ${isDarkTheme ? 'bg-shark-900' : 'bg-shark-50'}`} {...props} />
 }
 
 export default ThemedCard
